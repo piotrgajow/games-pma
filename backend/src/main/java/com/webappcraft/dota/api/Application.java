f6ad4f562fa31f521956da393133d80c2e06f5cf
Piotr Gajowniczek <piotrgajow@gmail.com>
2019-02-22 23:34:28 +0100

    Add base controller with hero post method

--- backend/src/main/java/com/webappcraft/dota/api/Application.java
+++ backend/src/main/java/com/webappcraft/dota/api/Application.java
@@ -2,8 +2,12 @@ package com.webappcraft.dota.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.webappcraft.dota.api"})
+@EnableJpaRepositories(basePackages = {"com.webappcraft.dota.storage.repository"})
+@EntityScan(basePackages = {"com.webappcraft.dota.storage.entity"})
 public class Application {
 
     public static void main(String[] args) {
--- backend/src/main/java/com/webappcraft/dota/api/controller/HeroController.java
+++ backend/src/main/java/com/webappcraft/dota/api/controller/HeroController.java
@@ -1,14 +1,26 @@
 package com.webappcraft.dota.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import com.webappcraft.dota.storage.entity.HeroEntity;
+import com.webappcraft.dota.storage.repository.HibernateHeroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HeroController {
 
-    @GetMapping("/")
-    public String index() {
-        return "Hello Spring!";
+    private final HibernateHeroRepository heroRepository;
+
+    @Autowired
+    public HeroController(HibernateHeroRepository heroRepository) {
+        this.heroRepository = heroRepository;
+    }
+
+    @PostMapping("/hero")
+    public void createHero(@RequestBody HeroEntity hero) {
+        System.out.println(hero.getName());
+        heroRepository.save(hero);
     }
 
 }

9f407181d4de4d47ffac68ea7fa91db17bfcee1d
Piotr Gajowniczek <piotrgajow@gmail.com>
2019-02-22 23:45:35 +0100

    Add update hero method

--- backend/src/main/java/com/webappcraft/dota/api/controller/HeroController.java
+++ backend/src/main/java/com/webappcraft/dota/api/controller/HeroController.java
@@ -3,7 +3,9 @@ package com.webappcraft.dota.api.controller;
 import com.webappcraft.dota.storage.entity.HeroEntity;
 import com.webappcraft.dota.storage.repository.HibernateHeroRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +21,12 @@ public class HeroController {
 
     @PostMapping("/hero")
     public void createHero(@RequestBody HeroEntity hero) {
-        System.out.println(hero.getName());
+        heroRepository.save(hero);
+    }
+
+    @PutMapping("/hero/{heroId}")
+    public void updateHero(@PathVariable("heroId") Long heroId, @RequestBody HeroEntity hero) {
+        hero.setId(heroId);
         heroRepository.save(hero);
     }
 
